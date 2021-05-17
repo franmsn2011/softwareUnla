9da74309b5cc154aea7ddf39f09e04670388bbe6
franmsn2011 <51340801+franmsn2011@users.noreply.github.com>
2021-05-17 19:12:21 -0300

    creo repositorio

---
+++ src/main/java/software/demo/ProyectCuatrimestralApplication.java
@@ -0,0 +1,13 @@
+package software.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectCuatrimestralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectCuatrimestralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/software/demo/ProyectCuatrimestralApplicationTests.java
@@ -0,0 +1,13 @@
+package software.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectCuatrimestralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
